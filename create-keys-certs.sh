0fde6f208ca2dec39648b56ced87347cbdb1beb8
Sebastian Dressler <dressler@mailbox.org>
2019-11-23 11:41:12 +0100

    Use pub host key

--- create-keys-certs.sh
+++ create-keys-certs.sh
@@ -13,10 +13,5 @@ echo "[root]" >> $aws_credentials_file
 echo "role_arn = arn:aws:iam::${account_id}:role/RootRole" >> $aws_credentials_file
 echo "credential_source = Ec2InstanceMetadata" >> $aws_credentials_file
 
-function copy_to_s3 {
-    aws2 s3 --no-sign-requres --region ${aws_region} \
-    cp $1 s3://${s3_bucket/$(basename $1)
-}
-
-ssh-keygen -b 2048 -t rsa -f /root/s64dacluster -q -N ""
-copy_to_s3 /root/s64dacluster.pub
+aws2 s3 --no-sign-request --region ${aws_region} \
+  cp /etc/ssh/ssh_host_rsa_key.pub s3://${s3_bucket}/frontend_key.pub
