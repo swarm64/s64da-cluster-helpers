abad2df369fcc66f2bf3d513a5af7f708e06edaa
Sebastian Dreßler <sebastian@swarm64.com>
2019-11-26 11:27:36 +0900

    Add node control script

---
+++ node-control.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+NODES_INFO=/var/nodes
+ASG_NAME=foo
+
+function print_help {
+  echo "Usage: $0 {attach|detach} <node-number>"
+}
+
+if [ -z ${1+x} ]; then
+  echo "Please provide a command."
+  print_help
+  exit -1
+else
+  CMD=$1
+fi
+
+if [ -z ${2+x} ]; then
+  echo "Please provide a node number."
+  print_help
+  exit -1
+else
+  NODE=$2
+fi
+
+NODE_ID=`awk -v num=$NODE '$1 == num {print $2}' $NODES_INFO`
+
+case $CMD in
+  "attach")
+    echo "Attaching node $NODE ($NODE_ID)"
+    aws2 start-instances --instance-ids $NODE_ID
+    aws2 autoscaling attach-instances --instance-ids $NODE_ID \
+      --auto-scaling-group-name $ASG_NAME
+    pcp_attach_node -h 127.0.0.1 -U pgpool -p 9898 -n $NODE -w
+    ;;
+  "detach")
+    echo "Detaching node $NODE ($NODE_ID)"
+    pcp_detach_node -h 127.0.0.1 -U pgpool -p 9898 -n $NODE -g
+    aws2 autoscaling detach-instances --instances-ids $NODE_ID \
+      --auto-scaling-group-name $ASG_NAME \
+      --should-decrement-desired-capacity
+    aws2 start-instances --instance-ids $NODE_ID
+    ;;
+  *)
+esac
