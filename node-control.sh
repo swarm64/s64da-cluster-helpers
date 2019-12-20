284881a04535f060b43a7ff121b852958fab99d0
Sebastian Dreßler <sebastian@swarm64.com>
2019-12-20 11:03:47 +0100

    Wait for PG, don't ask password on detach

--- node-control.sh
+++ node-control.sh
@@ -8,6 +8,29 @@ function print_help {
   echo "Usage: $0 {attach|detach} <node-number>"
 }
 
+function wait_for_pg {
+    set +e
+    PG_HOST=$1
+
+    PSQL_UP=0
+    for i in {0..60}; do
+        pg_isready -h $PG_HOST
+        if [ $? -eq 0 ]; then
+            PSQL_UP=1
+            break
+        fi
+        sleep 15
+    done
+
+    if [ $PSQL_UP -ne 1 ]; then
+        echo "PSQL did not start within time. Aborting."
+        exit -1
+    fi
+
+    set -e
+}
+
+
 if [ -z ${1+x} ]; then
   echo "Please provide a command."
   print_help
@@ -32,11 +55,14 @@ case $CMD in
     aws2 start-instances --instance-ids $NODE_ID
     aws2 autoscaling attach-instances --instance-ids $NODE_ID \
       --auto-scaling-group-name $ASG_NAME
+
+    wait_for_pg dn$NODE
+
     pcp_attach_node -h 127.0.0.1 -U pgpool -p 9898 -n $NODE -w
     ;;
   "detach")
     echo "Detaching node $NODE ($NODE_ID)"
-    pcp_detach_node -h 127.0.0.1 -U pgpool -p 9898 -n $NODE -g
+    pcp_detach_node -h 127.0.0.1 -U pgpool -p 9898 -n $NODE -g -w
     aws2 autoscaling detach-instances --instances-ids $NODE_ID \
       --auto-scaling-group-name $ASG_NAME \
       --should-decrement-desired-capacity
